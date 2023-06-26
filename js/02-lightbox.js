89c5a5ad836154f9a254b7453f2cf1980fd87215
DianaMotorna <124207230+DianaMotorna@users.noreply.github.com>
2023-06-26 14:10:28 +0300

    prevent images from downloading

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 
+const images = document.querySelector("ul.gallery");
+
+images.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
 new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionsData: "alt",
